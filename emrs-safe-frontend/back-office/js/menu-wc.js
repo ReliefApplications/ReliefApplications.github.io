748dd5fb1d3b431582e516aa90179ed6b48f124b
AntoineRelief <50317271+AntoineRelief@users.noreply.github.com>
2021-06-11 15:41:58 +0000

    Deploying to master from @ ReliefApplications/emrs-safe-frontend@ecd155f546477a7f3072eb61cc8a1204bf147a72 🚀

--- emrs-safe-frontend/back-office/js/menu-wc.js
+++ emrs-safe-frontend/back-office/js/menu-wc.js
@@ -216,13 +216,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/ApplicationToolbarModule.html" data-type="entity-link">ApplicationToolbarModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#components-links-module-ApplicationToolbarModule-5b316456cddae46f28aed9c22219c3b7"' : 'data-target="#xs-components-links-module-ApplicationToolbarModule-5b316456cddae46f28aed9c22219c3b7"' }>
+                                            'data-target="#components-links-module-ApplicationToolbarModule-b03804c1acf6be6546e7e41ee6d1afe0"' : 'data-target="#xs-components-links-module-ApplicationToolbarModule-b03804c1acf6be6546e7e41ee6d1afe0"' }>
                                             <span class="icon ion-md-cog"></span>
                                             <span>Components</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-ApplicationToolbarModule-5b316456cddae46f28aed9c22219c3b7"' :
-                                            'id="xs-components-links-module-ApplicationToolbarModule-5b316456cddae46f28aed9c22219c3b7"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-ApplicationToolbarModule-b03804c1acf6be6546e7e41ee6d1afe0"' :
+                                            'id="xs-components-links-module-ApplicationToolbarModule-b03804c1acf6be6546e7e41ee6d1afe0"' }>
                                             <li class="link">
                                                 <a href="components/ApplicationToolbarComponent.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">ApplicationToolbarComponent</a>
