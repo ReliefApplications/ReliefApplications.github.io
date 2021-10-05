e328542423c3c58b720d8e7c54f6c539326ad628
AntoineRelief <50317271+AntoineRelief@users.noreply.github.com>
2021-10-05 17:44:25 +0000

    Deploying to master from @ ReliefApplications/emrs-safe-frontend@a92c19a3f9fa44c4f63bef58b3b5f89da0e9be48 🚀

--- emrs-safe-frontend/back-office/js/menu-wc.js
+++ emrs-safe-frontend/back-office/js/menu-wc.js
@@ -338,7 +338,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                         <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-DashboardModule-9d4f32bef0025eb125fedb9fdac3d2b4-1"' :
                                             'id="xs-components-links-module-DashboardModule-9d4f32bef0025eb125fedb9fdac3d2b4-1"' }>
                                             <li class="link">
-                                                <a href="components/DashboardComponent-1.html"
+                                                <a href="components/DashboardComponent.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">DashboardComponent</a>
                                             </li>
                                         </ul>
@@ -1008,6 +1008,19 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                 </ul>
                 </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#components-links"' :
+                            'data-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/DashboardComponent-1.html" data-type="entity-link">DashboardComponent</a>
+                            </li>
+                        </ul>
+                    </li>
                         <li class="chapter">
                             <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
                                 'data-target="#xs-injectables-links"' }>
