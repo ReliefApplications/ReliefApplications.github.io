b162da4cb0655150f4a5d4192aa550ea43631bdd
AntoineRelief <50317271+AntoineRelief@users.noreply.github.com>
2021-11-09 16:13:31 +0000

    Deploying to master from @ ReliefApplications/emrs-safe-frontend@d21976a7ca9c172ad424c5edb9380d03650a9ca2 🚀

--- emrs-safe-frontend/back-office/js/menu-wc.js
+++ emrs-safe-frontend/back-office/js/menu-wc.js
@@ -192,13 +192,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/ApplicationsModule.html" data-type="entity-link">ApplicationsModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#components-links-module-ApplicationsModule-ca6f9f24ecbdb94d379e612d2be9bd89"' : 'data-target="#xs-components-links-module-ApplicationsModule-ca6f9f24ecbdb94d379e612d2be9bd89"' }>
+                                            'data-target="#components-links-module-ApplicationsModule-f3dc2eb328d70210a0ca758cd1b01220"' : 'data-target="#xs-components-links-module-ApplicationsModule-f3dc2eb328d70210a0ca758cd1b01220"' }>
                                             <span class="icon ion-md-cog"></span>
                                             <span>Components</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-ApplicationsModule-ca6f9f24ecbdb94d379e612d2be9bd89"' :
-                                            'id="xs-components-links-module-ApplicationsModule-ca6f9f24ecbdb94d379e612d2be9bd89"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-ApplicationsModule-f3dc2eb328d70210a0ca758cd1b01220"' :
+                                            'id="xs-components-links-module-ApplicationsModule-f3dc2eb328d70210a0ca758cd1b01220"' }>
                                             <li class="link">
                                                 <a href="components/ApplicationsComponent.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">ApplicationsComponent</a>
