590c0182316d3a2fb81dbad43d376550c8e047b1
AntoineRelief <50317271+AntoineRelief@users.noreply.github.com>
2021-11-16 12:40:02 +0000

    Deploying to master from @ ReliefApplications/emrs-safe-frontend@8f0fdbd8a64efe5f445d74e2cb592bfd3500b195 🚀

--- emrs-safe-frontend/web-element/js/menu-wc.js
+++ emrs-safe-frontend/web-element/js/menu-wc.js
@@ -65,13 +65,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#components-links-module-AppModule-eed2d279b1683e13c84d1a0240721f8e"' : 'data-target="#xs-components-links-module-AppModule-eed2d279b1683e13c84d1a0240721f8e"' }>
+                                            'data-target="#components-links-module-AppModule-febce8014bfd490c2443624179750a30"' : 'data-target="#xs-components-links-module-AppModule-febce8014bfd490c2443624179750a30"' }>
                                             <span class="icon ion-md-cog"></span>
                                             <span>Components</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-eed2d279b1683e13c84d1a0240721f8e"' :
-                                            'id="xs-components-links-module-AppModule-eed2d279b1683e13c84d1a0240721f8e"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-febce8014bfd490c2443624179750a30"' :
+                                            'id="xs-components-links-module-AppModule-febce8014bfd490c2443624179750a30"' }>
                                             <li class="link">
                                                 <a href="components/AppComponent.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppComponent</a>
